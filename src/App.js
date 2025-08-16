9f80bfc1a31138e6674b0cc4a959caded92ef97f
ritesh-chauhan0x1 <22054368@kiit.ac.in>
2025-08-17 04:31:24 +0530

    Initial commit: Chat-App

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to "Chat-App"</h1>
+        <p>"Real-time messaging with file sharing"</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
